acec16377f6a3459be5a6848d214b1d6c85bef00
Marina <mbakanova2016@yandex.ru>
2023-07-26 19:58:31 +0300

    Try to autoshoot

---
+++ Assets/Scripts/AutoShooter.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoShooter : MonoBehaviour
+{
+    [SerializeField] private float _attackRadius = 5f;
+    [SerializeField] private float _fireRate = 0.5f;
+    [SerializeField] private int _damage = 10;
+    [SerializeField] private float _bulletSpeed = 10f;
+    [SerializeField] private GameObject _bullet;
+    private readonly List<Transform> _enemies = new();
+
+    private float _nextFireTime;
+    private Transform _target;
+
+    private void Start()
+    {
+        gameObject.GetComponent<CircleCollider2D>().radius = _attackRadius;
+    }
+
+    private void Update()
+    {
+        if (_target is null && _enemies.Count > 0)
+        {
+            _target = _enemies[0];
+            _enemies.RemoveAt(0);
+            RotateTowardsEnemy();
+        }
+
+
+        if (!CanFire()) return;
+        
+        Fire();
+        ResetFireTimer();
+        Debug.Log(_target.gameObject.name);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (_target == null && other.CompareTag($"Enemy"))
+        {
+            _target = other.transform;
+            RotateTowardsEnemy();
+        }
+        else if (other.CompareTag($"Enemy"))
+        {
+            _enemies.Add(other.transform);
+        }
+    }
+
+    private void RotateTowardsEnemy()
+    {
+        var direction = (_target.position - transform.position).normalized;
+        var lookRotation = Quaternion.LookRotation(new Vector2(direction.x, 0f));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 10f);
+    }
+
+    private bool CanFire()
+    {
+        return Time.time >= _nextFireTime;
+    }
+    
+    private void Fire()
+    {
+        var bullet =
+            Instantiate(_bullet, transform.position, Quaternion.identity);
+        var bulletController = bullet?.GetComponent<BulletController>();
+
+        if (bulletController is not null)
+        {
+            BulletController.Instance.MoveBullet(_target, _bulletSpeed);
+        }
+
+        _nextFireTime = Time.time + _fireRate;
+    }
+
+    private void ResetFireTimer()
+    {
+        _nextFireTime = Time.time + _fireRate;
+    }
+}
---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public static BulletController Instance { get; private set; }
+
+    private void Start()
+    {
+        Instance = this;
+    }
+
+    public void MoveBullet(Transform target, float bulletSpeed)
+    {
+        var direction = (target.position - transform.position).normalized;
+        GetComponent<Rigidbody2D>().velocity = direction * bulletSpeed;
+    }
+}
